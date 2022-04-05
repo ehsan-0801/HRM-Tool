f2a215bfbad792bfdba4a4fb69ea5e6b83e0a8f1
ehsan-0801 <ehsanulahmmed@gmail.com>
2022-04-05 15:07:20 +0600

    Updated Again

--- emanage/attendance_list.php
+++ emanage/attendance_list.php
@@ -16,44 +16,62 @@ include_once('header.php')
 <body>
     <section class="container">
         <div class="row">
-            <div class="my-2 mx-md-5 p-3 border border-1 border-secondary rounded col-md-5 col-sm-12">
+            <div class="text-light my-2 mx-md-5 p-3 border border-1 border-secondary rounded col-md-5 col-sm-12" style="background: #ddb892;">
                 <form action="checkDateWIse.php" method="POST" class="px-md-5">
-                    Date from:
-                    <input type="date" name="fromDate" id="">
-                    <br>
-                    Date to:
-                    <input type="date" name="toDate" id="">
-                    <br>
-                    <input class="btn btn-outline-primary px-3" type="submit" name="submit" value="Find">
+                    <div class="mb-3 row">
+                        <label for="dateFrom" class="col-sm-2 col-form-label">Date from:</label>
+                        <div class="col-sm-10">
+                            <input type="date" class="w-100 p-2 border border-3 border-light rounded" name="fromDate" id="">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="dateTo" class="col-sm-2 col-form-label">Date To:</label>
+                        <div class="col-sm-10">
+                            <input type="date" class="w-100 p-2 border border-3 border-light rounded" name="toDate" id="">
+                        </div>
+                    </div>
+                    <div class="w-25">
+                        <input type="submit" class="btn btn-primary p-2 border border-5 border-primary rounded w-100" value="Find">
+                    </div>
+
                 </form>
             </div>
-            <div class="my-2 mx-md-5 p-3 border border-1 border-secondary rounded col-md-5 col-sm-12">
-            <form action="checkemployeeWise.php" method="POST">
-                    Employee name:
-                    <select name="name" id="name">
-                <?php
-                $sql = 'SELECT * FROM employee_info';
-                $result = $conn->query($sql);
-                if (mysqli_num_rows($result) < 0){
-                    echo 'No value Found';
-                }
-                else{
-                    while($row = $result->fetch_assoc()) {
-                        echo "
-                        <option value='".$row["id"]."' name='' >".$row["name"]."</option>";
-                      
-                    }
-            
-                }
-                ?>
-                </select>
-                <br>
-                Date from:
-                <input type="date" name="fromDate" id=""> <br>
-                Date to:
-                <input type="date" name="toDate" id="">
-                <br>
-                <input class="btn btn-outline-primary px-3" type="submit" value="Find">
+            <div class="my-2 mx-md-5 p-3 border border-1 border-secondary rounded col-md-5 col-sm-12 text-light" style="background: #0a9396;">
+                <form action="checkemployeeWise.php" method="POST">
+                    <div class="mb-3 row">
+                        <label for="staticEmail" class="col-sm-2 col-form-label">Name</label>
+                        <div class="col-sm-10">
+                            <select class="p-2 w-100 border border-3 border-light rounded" name="name" id="name">
+                                <?php
+                                $sql = 'SELECT * FROM employee_info';
+                                $result = $conn->query($sql);
+                                if (mysqli_num_rows($result) < 0) {
+                                    echo 'No value Found';
+                                } else {
+                                    while ($row = $result->fetch_assoc()) {
+                                        echo "
+                        <option value='" . $row["id"] . "' name='' >" . $row["name"] . "</option>";
+                                    }
+                                }
+                                ?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="dateFrom" class="col-sm-2 col-form-label">Date from:</label>
+                        <div class="col-sm-10">
+                            <input type="date" class="w-100 p-2 border border-3 border-light rounded" name="fromDate" id="">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="dateTo" class="col-sm-2 col-form-label">Date To:</label>
+                        <div class="col-sm-10">
+                            <input type="date" class="w-100 p-2 border border-3 border-light rounded" name="toDate" id="">
+                        </div>
+                    </div>
+                    <div class="w-25">
+                        <input type="submit" class="btn btn-secondary p-2 border border-5 border-secondary rounded w-100" value="Find">
+                    </div>
                 </form>
             </div>
         </div>
--- emanage/checkemployeeWise.php
+++ emanage/checkemployeeWise.php
@@ -69,6 +69,262 @@ include('header.php');
     </table>
     <span>Present Days: </span><?php echo $row_count ?>
 </div>
+<div class="container">
+    <table class="table table-success table-striped table-hover my-5">
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Leave Taken From</th>
+                <th scope="col">Leave Ended</th>
+                <th scope="col">Reason</th>
+                <th scope="col">Notes</th>
+                <th scope="col">Total Leave</th>
+            </tr>
+        </thead>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $id = $_POST['name'];
+            $dateFrom = $_POST['fromDate'];
+            $dateTo = $_POST['toDate'];
+            // echo $id;
+            $sql = "SELECT * FROM leave_manage,employee_info WHERE leave_manage.employee_id = '$id' AND leave_manage.employee_id = employee_info.id AND leave_dt_from BETWEEN '$dateFrom' AND '$dateTo'";
+            $result = $conn->query($sql);
+
+            $new_name = true;
+
+            if ($result) {
+
+                if ($result->num_rows > 0) {
+                    $row_count = $result->num_rows;
+                    $total_leave = 0;
+                    while ($row = $result->fetch_assoc()) {
+
+                        $leave_taken = date_diff(
+                            date_create($row['leave_dt_to']),
+                            date_create($row['leave_dt_from'])
+                        );
+
+                        $leave_taken = 1 + $leave_taken->format('%a');
+
+                        if ($row['leave_dt_from_fh'] == "Half")
+                            $leave_taken -= 0.5;
+
+                        if ($row['leave_dt_to_fh'] == "Half")
+                            $leave_taken -= 0.5;
+
+                        if ($row['leave_dt_to'] == $row['leave_dt_from']) {
+                            if ($row['leave_dt_from_fh'] == "Full" || $row['leave_dt_to_fh'] == "Full") {
+                                $leave_taken = 1;
+                            } elseif ($row['leave_dt_from_fh'] == "Half" || $row['leave_dt_to_fh'] == "Half") {
+                                $leave_taken = .5;
+                            }
+                        }
+                        if ($new_name) {
+                            $name = "<td class='px-5 py-3 border border-1 border-secondary rounded' rowspan='$row_count'>
+                      $row[name]
+                    </td>";
+                            $new_name = false;
+                        }
+
+
+                        echo "
+                            <tr>
+                                $name
+                                <td class='px-5 py-3 border border-1 border-secondary rounded'>
+                                $row[leave_dt_from]
+                                </td>
+                                <td class='px-5 py-3 border border-1 border-secondary rounded'>
+                                $row[leave_dt_to]
+                                </td>
+                                <td class='px-5 py-3 border border-1 border-secondary rounded'>
+                                $row[type]
+                                </td>
+                                <td class='px-5 py-3 border border-1 border-secondary rounded'>
+                                $row[notes]
+                                </td>
+                                <td class='px-5 py-3 border border-1 border-secondary rounded'>$leave_taken</td>
+                                
+                            </tr>
+                            
+                            ";
+
+                        $name = '';
+                        if ($leave_taken == 0) {
+                            $total_leave = $leave_taken;
+                        } else {
+                            $total_leave = $total_leave + $leave_taken;
+                        }
+                    }
+                } else {
+                    echo $total_leave, " leave taken";
+                }
+            } else {
+                echo "Error in " . $query . "<br>" . $conn->error;
+            }
+        }
+
+        echo "<tr>
+                                
+                                <td class='px-5 py-3 border border-1 border-secondary rounded' colspan='5'>
+                                
+                                </td>
+                                <td class='px-5 py-3 border border-1 border-secondary rounded'>
+                                <span class='fw-bold text-primary'>TOTAL:</span> $total_leave
+                                </td>
+                            </tr>";
+        ?>
+
+    </table>
+    <table class="table table-info table-striped table-hover my-5 w-50 mx-auto">
+
+        <tbody>
+            <?php
+            $sql = "SELECT * FROM leave_manage,employee_info WHERE leave_manage.employee_id = '$id' AND leave_manage.employee_id = employee_info.id";
+            $result = $conn->query($sql);
+            $total_medical_leave = 14.00;
+            $total_casual_leave = 10.00;
+            $total_Festival_leave = 10.00;
+            $medical_leave = 0;
+            $casual_leave = 0;
+            $Festival_leave = 0;
+            $extra = 0;
+
+            if (mysqli_num_rows($result) < 0) {
+                echo "No records found";
+            } else {
+                while ($row = $result->fetch_assoc()) {
+
+                    if ($row['type'] == 'Medical Leave') {
+                        $leave_taken_medical = date_diff(
+                            date_create($row['leave_dt_to']),
+                            date_create($row['leave_dt_from'])
+                        );
+                        $leave_taken_medical = 1 + $leave_taken_medical->format('%a');
+                        if ($row['leave_dt_from_fh'] == "Half")
+                            $leave_taken_medical -= 0.5;
+
+                        if ($row['leave_dt_to_fh'] == "Half")
+                            $leave_taken_medical -= 0.5;
+
+                        if ($row['leave_dt_to'] == $row['leave_dt_from']) {
+                            if ($row['leave_dt_from_fh'] == "Full" || $row['leave_dt_to_fh'] == "Full") {
+                                $leave_taken_medical = 1;
+                            } elseif ($row['leave_dt_from_fh'] == "Half" || $row['leave_dt_to_fh'] == "Half") {
+                                $leave_taken_medical = .5;
+                            }
+                        }
+
+
+                        $medical_leave +=
+                            $leave_taken_medical;
+                    }
+                    if ($row['type'] == 'Casual Leave') {
+                        $leave_taken_casual = date_diff(
+                            date_create($row['leave_dt_to']),
+                            date_create($row['leave_dt_from'])
+                        );
+                        $leave_taken_casual = 1 + $leave_taken_casual->format('%a');
+                        if ($row['leave_dt_from_fh'] == "Half")
+                            $leave_taken_casual -= 0.5;
+
+                        if ($row['leave_dt_to_fh'] == "Half")
+                            $leave_taken_casual -= 0.5;
+
+                        if ($row['leave_dt_to'] == $row['leave_dt_from']) {
+                            if ($row['leave_dt_from_fh'] == "Full" || $row['leave_dt_to_fh'] == "Full") {
+                                $leave_taken_casual = 1;
+                            } elseif ($row['leave_dt_from_fh'] == "Half" || $row['leave_dt_to_fh'] == "Half") {
+                                $leave_taken_casual = .5;
+                            }
+                        }
+
+
+                        $casual_leave +=
+                            $leave_taken_casual;
+                    }
+                    if ($row['type'] == 'Festival leave') {
+                        $leave_taken_festival = date_diff(
+                            date_create($row['leave_dt_to']),
+                            date_create($row['leave_dt_from'])
+                        );
+                        $leave_taken_festival = 1 + $leave_taken_festival->format('%a');
+                        if ($row['leave_dt_from_fh'] == "Half")
+                            $leave_taken_festival -= 0.5;
+
+                        if ($row['leave_dt_to_fh'] == "Half")
+                            $leave_taken_festival -= 0.5;
+
+                        if ($row['leave_dt_to'] == $row['leave_dt_from']) {
+                            if ($row['leave_dt_from_fh'] == "Full" || $row['leave_dt_to_fh'] == "Full") {
+                                $leave_taken_festival = 1;
+                            } elseif ($row['leave_dt_from_fh'] == "Half" || $row['leave_dt_to_fh'] == "Half") {
+                                $leave_taken_festival = .5;
+                            }
+                        }
+
+
+                        $Festival_leave +=
+                            $leave_taken_festival;
+                    }
+                    if ($row['type'] == 'Other') {
+                        $leave_taken_festival = date_diff(
+                            date_create($row['leave_dt_to']),
+                            date_create($row['leave_dt_from'])
+                        );
+                        $leave_taken_festival = 1 + $leave_taken_festival->format('%a');
+                        if ($row['leave_dt_from_fh'] == "Half")
+                            $leave_taken_festival -= 0.5;
+
+                        if ($row['leave_dt_to_fh'] == "Half")
+                            $leave_taken_festival -= 0.5;
+
+                        if ($row['leave_dt_to'] == $row['leave_dt_from']) {
+                            if ($row['leave_dt_from_fh'] == "Full" || $row['leave_dt_to_fh'] == "Full") {
+                                $leave_taken_festival = 1;
+                            } elseif ($row['leave_dt_from_fh'] == "Half" || $row['leave_dt_to_fh'] == "Half") {
+                                $leave_taken_festival = .5;
+                            }
+                        }
+
+
+                        $Festival_leave +=
+                            $leave_taken_festival;
+                    }
+                }
+                $total_medical_leave = $total_medical_leave - $medical_leave;
+                $total_casual_leave = $total_casual_leave - $casual_leave;
+                $total_Festival_leave = $total_Festival_leave - $Festival_leave;
+
+                echo "
+                <thead>
+            <tr>
+                <th scope='col' class='text-center text-primary'>Leave Type</th>
+                <th scope='col' class='text-center text-primary'>Remaining Leave</th>
+            </tr>
+        </thead>
+                  <tr>
+                <th scope='row' class='text-center'>Medical Leave</th>
+                <td class='text-center'>$total_medical_leave</td>
+            </tr>
+                  <tr>
+                <th scope='row' class='text-center'>Casual Leave</th>
+                <td class='text-center'>$total_casual_leave</td>
+            </tr>
+            </tr>
+                  <tr>
+                <th scope='row' class='text-center'>Festival Leave</th>
+                <td class='text-center'>$total_Festival_leave</td>
+            </tr>
+                ";
+            }
+            ?>
+
+
+
+        </tbody>
+    </table>
+</div>
 <?php
 include('footer.php')
 ?>
